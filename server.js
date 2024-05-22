e24c5aaf288aecf206ba286c5fd960ac84aa6d52
unknown <fer.romero1991@gmail.com>
2024-05-22 20:07:45 -0300

    add basic mongo connected

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
+const dbURI =
+  process.env.MONGO_URI || "mongodb://root:example@db:27017/utn_devops";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
 });
 
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+const User = mongoose.model('User', userSchema);
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) res.status(500).send(err);
+    res.json(users);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("¡Hola Mundo!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
