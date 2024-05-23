a9f09a8e1aeef44d7414c71e57a5f2fa30356576
unknown <fer.romero1991@gmail.com>
2024-05-22 21:11:18 -0300

    fix

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
 
-const dbURI =
-  process.env.MONGO_URI || "mongodb://root:example@db:27017/utn_devops";
+const dbURI = process.env.MONGO_URI || 'mongodb://root:yari@db:27017/utn_devops';
+console.log(`Connecting to MongoDB at ${dbURI}`);
 
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: String
 });
 
 const User = mongoose.model('User', userSchema);
 
 app.get('/api/users', (req, res) => {
   User.find({}, (err, users) => {
-    if (err) res.status(500).send(err);
-    res.json(users);
+    if (err) {
+      console.log('Error fetching users:', err);
+      res.status(500).send(err);
+    } else {
+      res.json(users);
+    }
   });
 });
-
 app.get("/", (req, res) => {
   res.send("¡Hola Mundo!");
 });
